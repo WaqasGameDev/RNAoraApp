25c2a4b1a60faccec8da219cdba984f2fd814bff
WaqasHaider <engr.waqashaider@gmail.com>
2024-10-03 20:40:14 +0500

    more elements added to landing screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import {View, ScrollView, Image } from 'react-native'
+import { View, ScrollView, Image, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../constants'
@@ -6,11 +6,30 @@ import { images } from '../constants'
 const App = () => {
   return (
     <SafeAreaView className='bg-primary h-full'>
-      <ScrollView contentContainerStyle={{height:"100%"}}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className='w-full h-full items-center px-4'>
-            <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode='contain'>
-              
-            </Image>
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain' />
+          <Image
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode='contain' />
+          <View className='relative mt-5'>
+            <Text className="text-white text-3xl text-center font-bold">
+              Discover Endless Possibilities with{' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+              resizeMode='contain'
+            />
+          </View>
+          <Text className='text-gray-100 text-sm font-pregular mt-7 text-center'>
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
