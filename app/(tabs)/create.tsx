e95e5a8d7f25fe794ac272ee14c8ba536fd4831e
WaqasHaider <engr.waqashaider@gmail.com>
2024-10-09 15:56:37 +0500

    uploading working fine

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,25 +1,105 @@
-import { View, Text, ScrollView, TouchableOpacity,Image } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { CustomButton, FormField } from '../../components'
 import { Video, ResizeMode, AVPlaybackStatus, AVPlaybackStatusSuccess } from 'expo-av'
 import { icons } from '../../constants'
+import * as ImagePicker from 'expo-image-picker'
+import { router } from 'expo-router'
+import { createVideo } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Create = () => {
-const[form, setForm] = useState(
-  {
-    title:'',
-    video:'',
-    thumbnail:'',
-    prompt:''
+
+  type ImagePickerAsset = {
+    assetId?: string | null
+    base64?: string | null
+    duration?: number | null
+    exif?: Record<string, any> | null
+    filename?: string | null
+    filesize?: number | null
+    height?: number | null
+    mimetype?: string | null
+    type?: string | null
+    uri?: string | null
+    width?: number | null
+
   }
-)
 
-const [uploading, setUploading] = useState(false)
+  type FormState = {
+    title: string;
+    video: ImagePickerAsset | null;
+    thumbnail: ImagePickerAsset | null;
+    prompt: string;
+    userId: string
+  }
 
-const submit = async ()=>{
+  const {user} = useGlobalContext();
 
-}
+  const [form, setForm] = useState<FormState>({
+    title: '',
+    video: null,
+    thumbnail: null,
+    prompt: '',
+    userId: ''
+  });
+
+  const [uploading, setUploading] = useState(false)
+
+  const openPicker = async (selectType: string) => {
+    const result = await ImagePicker.launchImageLibraryAsync(
+      {
+        mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing: false,
+        quality: 1,
+      }
+    )
+
+    if (!result.canceled) {
+      if (selectType === 'image') {
+        setForm({ ...form, thumbnail: result.assets[0] })
+      }
+
+      if (selectType === 'video') {
+        setForm({ ...form, video: result.assets[0] })
+      }
+    }
+    else {
+      setTimeout(() => {
+        Alert.alert('Document Picked', JSON.stringify(
+          result, null, 2
+        ))
+      }, 100);
+    }
+  }
+
+  const submit = async () => {
+    if (!form.prompt || !form.thumbnail || !form.title || !form.video) {
+      return Alert.alert('Please fill in all the fields')
+    }
+
+    setForm((prevForm) => {
+      const updatedForm = { ...prevForm, userId: user?.$id };
+      return updatedForm;
+    });
+
+    try {
+      setUploading(true)
+      await createVideo({
+        title: form.title,
+        thumbnail: form.thumbnail,
+        video: form.video,          
+        prompt: form.prompt,
+        userId: user?.$id
+    });
+      Alert.alert('Success', 'Post uploaded successfuly')
+      router.push('/home')
+    } catch (error) {
+      Alert.alert('Error', error instanceof Error ? error.message : String(error))
+    } finally {
+      setUploading(false)
+    }
+  }
 
   return (
     <SafeAreaView className='bg-primary h-full'>
@@ -27,78 +107,79 @@ const submit = async ()=>{
         <Text className='text-2xl text-white font-psemibold'>
           Upload Video
         </Text>
-        <FormField 
-        title='Video Title'
-        value={form.title}
-        placeholder='Give your video a catchy title...'
-        handleChangeText={(e)=>{setForm({...form,title:e})}}
-        otherStyles='mt-10'
+        <FormField
+          title='Video Title'
+          value={form.title}
+          placeholder='Give your video a catchy title...'
+          handleChangeText={(e) => { setForm({ ...form, title: e }) }}
+          otherStyles='mt-10'
         />
         <View className='mt-7 space-y-2'>
           <Text className='text-base text-gray-100 font-pmedium'>
             Upload Video
           </Text>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => openPicker('video')}
+          >
             {form.video ? (
-              <Video source={{uri: form.video}}
-                 className='w-full h-64 rounded-2xl'
-                 resizeMode={ResizeMode.COVER}
-                 shouldPlay
-                 isLooping
-                />
-            ) 
-            :
-             (
-              <View className='w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center'>
-                <View className='w-14 h-14 border border-dashed border-secondary-100 justify-center items-center'>
-                  <Image
-                  source={icons.upload}
-                  resizeMode='contain'
-                  className='w-1/2 h-1/2'
-                  />
+              <Video source={{ uri: form.video.uri ? form.video.uri : '' }}
+                className='w-full h-64 rounded-2xl'
+                resizeMode={ResizeMode.COVER}
+              />
+            )
+              :
+              (
+                <View className='w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center'>
+                  <View className='w-14 h-14 border border-dashed border-secondary-100 justify-center items-center'>
+                    <Image
+                      source={icons.upload}
+                      resizeMode='contain'
+                      className='w-1/2 h-1/2'
+                    />
+                  </View>
                 </View>
-              </View>
-            )}
+              )}
           </TouchableOpacity>
         </View>
         <View className='mt-7 space-y-2'>
-        <Text className='text-base text-gray-100 font-pmedium'>
+          <Text className='text-base text-gray-100 font-pmedium'>
             Upload Thumbnail
           </Text>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => openPicker('image')}>
             {form.thumbnail ? (
-              <Image source={{uri: form.thumbnail}}
-                 className='w-full h-64 rounded-2xl'
-                 resizeMode='cover'
-                />
-            ) 
-            :
-             (
-              <View className='w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2'>
+              <Image source={{ uri: form.thumbnail.uri ? form.thumbnail.uri : '' }}
+                className='w-full h-64 rounded-2xl'
+                resizeMode='cover'
+              />
+            )
+              :
+              (
+                <View className='w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2'>
                   <Image
-                  source={icons.upload}
-                  resizeMode='contain'
-                  className='w-5 h-5'
+                    source={icons.upload}
+                    resizeMode='contain'
+                    className='w-5 h-5'
                   />
                   <Text className='text-sm text-gray-100 font-pmedium'>
                     Choose a file
                   </Text>
-              </View>
-            )}
+                </View>
+              )}
           </TouchableOpacity>
         </View>
-        <FormField 
-        title='AI Propmt'
-        value={form.prompt}
-        placeholder='Give prompt you used to generate this video'
-        handleChangeText={(e)=>{setForm({...form,prompt:e})}}
-        otherStyles='mt-7'
+        <FormField
+          title='AI Prompt'
+          value={form.prompt}
+          placeholder='Prompt you used to generate this video'
+          handleChangeText={(e) => { setForm({ ...form, prompt: e }) }}
+          otherStyles='mt-7'
         />
         <CustomButton
-        title='Submit & Publish'
-        handlePressed={submit}
-        containerStyles='mt-7'
-        isLoading={uploading}
+          title='Submit & Publish'
+          handlePressed={submit}
+          containerStyles='mt-7'
+          isLoading={uploading}
         />
       </ScrollView>
     </SafeAreaView>
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,5 @@
-import { Client, Account, ID, Avatars, Databases, Query } from 'react-native-appwrite';
+import { Alert } from 'react-native';
+import { Client, Account, ID, Avatars, Databases, Query, Storage, ImageGravity } from 'react-native-appwrite';
 
 export const config = {
     endpoint: 'https://cloud.appwrite.io/v1',
@@ -30,12 +31,37 @@ client
 const account = new Account(client);
 const avatars = new Avatars(client);
 const databases = new Databases(client)
+const storage = new Storage(client)
 
 type CreateUserProps = {
     username: string
     email: string
     password: string
 }
+
+type ImagePickerAsset = {
+    assetId?:string | null
+    base64?:string | null
+    duration?:number | null
+    exif?:Record<string, any> | null
+    filename?:string | null
+    filesize?:number | null
+    height?:number | null
+    mimetype?:string | null
+    type?:string | null
+    uri?:string | null
+    width?:number | null
+
+}
+
+type UploadFormProps = {
+  title: string;
+  video: ImagePickerAsset;
+  thumbnail: ImagePickerAsset;
+  prompt: string;
+  userId:string
+}
+
 
 export const createUser = async ({ username, email, password }: CreateUserProps) => {
     try {
@@ -82,21 +108,21 @@ export const signIn = async ({ email, password }: SignInProps) => {
     }
 }
 
-export const getCurrentUser = async ()=>{
+export const getCurrentUser = async () => {
     try {
         const currentAccount = await account.get();
 
-        if(!currentAccount){
+        if (!currentAccount) {
             throw Error
         }
 
         const currentUser = await databases.listDocuments(
             dababase,
             usersCollection,
-           [Query.equal('accountId',currentAccount.$id)]
+            [Query.equal('accountId', currentAccount.$id)]
         )
 
-        if(!currentUser){
+        if (!currentUser) {
             throw Error
         }
 
@@ -108,71 +134,160 @@ export const getCurrentUser = async ()=>{
     }
 }
 
-export const getAllPosts = async ()=>{
+export const getAllPosts = async () => {
     try {
-       const posts = databases.listDocuments(
-        dababase,
-        videoCollection
-       )
+        const posts = databases.listDocuments(
+            dababase,
+            videoCollection
+        )
 
-       return (await posts).documents
+        return (await posts).documents
     } catch (error) {
         console.error(error)
         throw new Error(error instanceof Error ? error.message : String(error));
     }
 }
 
-export const getTrendingPosts = async ()=>{
+export const getTrendingPosts = async () => {
     try {
-       const posts = databases.listDocuments(
-        dababase,
-        videoCollection,
-        [Query.orderDesc('$createdAt'), Query.limit(7)]
-       )
+        const posts = databases.listDocuments(
+            dababase,
+            videoCollection,
+            [Query.orderDesc('$createdAt'), Query.limit(7)]
+        )
 
-       return (await posts).documents
+        return (await posts).documents
     } catch (error) {
         console.error(error)
         throw new Error(error instanceof Error ? error.message : String(error));
     }
 }
 
-export const searchPosts = async (query:string)=>{
+export const searchPosts = async (query: string) => {
     try {
-       const posts = databases.listDocuments(
-        dababase,
-        videoCollection,
-        [Query.search('title', query)]
-       )
+        const posts = databases.listDocuments(
+            dababase,
+            videoCollection,
+            [Query.search('title', query)]
+        )
 
-       return (await posts).documents
+        return (await posts).documents
     } catch (error) {
         console.error(error)
         throw new Error(error instanceof Error ? error.message : String(error));
     }
 }
 
-export const getUserPosts = async (userId:string)=>{
+export const getUserPosts = async (userId: string) => {
+
+    try {
+        const posts = databases.listDocuments(
+            dababase,
+            videoCollection,
+            [Query.equal('creator', userId)]
+        )
+
+        return (await posts).documents
+    } catch (error) {
+        console.error(error)
+        throw new Error(error instanceof Error ? error.message : String(error));
+    }
+}
+
+export const signout = async () => {
+
+    try {
+        const session = await account.deleteSession('current')
+        return session
+    } catch (error) {
+        console.error(error)
+        throw new Error(error instanceof Error ? error.message : String(error));
+    }
+}
+
+export const getFilePreview = async (fileId:string, fileType:string)=> {
+    let fileUrl;
     
     try {
-       const posts = databases.listDocuments(
-        dababase,
-        videoCollection,
-        [Query.equal('creator', userId)]
-       )
 
-       return (await posts).documents
+        if(fileType === 'video'){
+            fileUrl = storage.getFileView(storageId, fileId)
+        } else if (fileType === 'image'){
+            fileUrl = storage.getFilePreview(storageId, fileId, 2000, 2000, ImageGravity.Top,100)
+        }
+        else{
+            throw new Error(' Invalid file type')
+        }
+        
+        if(!fileUrl)
+            throw Error
+
+        return fileUrl
+
     } catch (error) {
         console.error(error)
         throw new Error(error instanceof Error ? error.message : String(error));
     }
+
 }
 
-export const signout = async ()=>{
-    
+export const uploadFile = async (file:ImagePickerAsset,fileType:string)=> {
+
+    const {
+        filename,   
+        mimetype,               
+        filesize,                      
+        uri,
+    } = file;
+
+    const asset = {
+        name: filename ?? `file-${Date.now()}`, 
+        type: mimetype ?? fileType,            
+        size: filesize ?? 0,                    
+        uri: uri ?? '',                                     
+    };
     try {
-       const session = await account.deleteSession('current')
-       return session
+
+        const uploadFile = await storage.createFile(
+            storageId,
+            ID.unique(),
+            asset
+        )
+        
+        const fileUrl = await getFilePreview(uploadFile.$id, fileType)
+
+        return fileUrl
+
+    } catch (error) {
+        console.error(error)
+        throw new Error(error instanceof Error ? error.message : String(error));
+    }
+
+}
+
+
+export const createVideo = async ({title, thumbnail, video, prompt, userId}:UploadFormProps) => {
+   
+    try {
+        const [videoUrl, thumbnailUrl] = await Promise.all([
+            uploadFile(thumbnail, 'image'),
+            uploadFile(video, 'video')
+        ])
+        const newPost = await databases.createDocument(
+            dababase,
+            videoCollection,
+            ID.unique(),
+            {
+                title:title,
+                thumbnail:thumbnailUrl,
+                video:videoUrl,
+                prompt:prompt,
+                creator: userId
+            }
+        )
+
+        return newPost
+
     } catch (error) {
         console.error(error)
         throw new Error(error instanceof Error ? error.message : String(error));
